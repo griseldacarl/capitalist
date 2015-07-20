5e02cd022582caa3571036a894bd2d57d1eac0c7
brgeren <brgeren@yahoo.com>
2015-07-20 14:26:07 -0700

    testtest

--- capitalist/GameViewController.swift
+++ capitalist/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // zzzz
+        // zzzzz
         // create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.dae")!
         
