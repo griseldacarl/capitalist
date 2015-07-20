adfaa4e0bc5b456488cbaeaafc869ce3079550bb
brgeren <brgeren@yahoo.com>
2015-07-20 14:19:18 -0700

    Added zzzz

--- capitalist/GameViewController.swift
+++ capitalist/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //
+        // zzzz
         // create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.dae")!
         
