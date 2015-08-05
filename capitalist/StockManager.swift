bfe229f8388d32558188f8d1b1a8d3e46eb75fcf
brgeren <brgeren@yahoo.com>
2015-08-05 12:11:18 -0700

    ??

---
+++ capitalist/StockManager.swift
@@ -0,0 +1,31 @@
+//
+//  StockManager.swift
+//  capitalist
+//
+//  Created by Brad on 7/30/15.
+//  Copyright (c) 2015 Carl Mitchell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StockManager:NSObject {
+    var listOfHotel1:[TileModel] = []
+    var listOfHotel2:[TileModel] = []
+    var listOfHotel3:[TileModel] = []
+    var listOfHotel4:[TileModel] = []
+    var listOfHotel5:[TileModel] = []
+    var listOfHotel6:[TileModel] = []
+    var listOfHotel7:[TileModel] = []
+    var availHotel1Count:Int
+    var availHotel2Count:Int
+    var availHotel3Count:Int
+    var availHotel4Count:Int
+    var availHotel5Count:Int
+    var availHotel6Count:Int
+    var availHotel7Count:Int
+    
+    func createStock() {
+    }
+
+}
